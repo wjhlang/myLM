7de77c1b23566379d3cf91b81374c708cfdebace
wjhlang <wjhlang@gmail.com>
2021-12-03 22:00:21 -0500

    Adding simulated datasets

---
+++ R/x.R
@@ -0,0 +1,8 @@
+#' Randomly generated x for linear regression modeling
+#'
+#' A dataset containing the randomly generated x.
+#'
+#' @format A matrix with 10000 rows and 5 variables generated for the normal distribution with mean = 0 and sd = 1:
+#'
+#'
+"x"
