c898638e9e81c737aa11d6d8dc5b4caf211c5bfa
wjhlang <wjhlang@gmail.com>
2021-12-03 22:26:14 -0500

    Update simulated dataset & vignettes

--- R/x.R
+++
@@ -1,8 +0,0 @@
-#' Randomly generated x for linear regression modeling
-#'
-#' A dataset containing the randomly generated x.
-#'
-#' @format A matrix with 10000 rows and 5 variables generated for the normal distribution with mean = 0 and sd = 1:
-#'
-#'
-"x"
